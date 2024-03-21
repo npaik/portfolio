559254598c9cf44a584b4f5b152b180295f1ce21
Tony Paik <nakhyunpaik@gmail.com>
2024-03-21 14:35:49 -0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
-
 import { twMerge } from "tailwind-merge";
 import NavBar from "./navBar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
