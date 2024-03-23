7d3f046138907367e303a4be1d5af0923bf2a3c6
Tony Paik <nakhyunpaik@gmail.com>
2024-03-23 14:30:37 -0700

    contact

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -3,11 +3,13 @@
  * @see https://v0.dev/t/kOso890NvLk
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
+
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Component() {
   return (
@@ -50,98 +52,48 @@ export default function Component() {
             </p>
             <div className="flex items-center space-x-4">
               <Link
-                href="https://www.threads.net/@nh.paik"
-                className="rounded-full p-2 hover:bg-gray-100"
-              >
-                <img src="/thread.png" alt="Thread" className="h-6 w-6" />
-                <span className="sr-only">Thread</span>
-              </Link>
-
-              <Link
                 className="rounded-full p-2 hover:bg-gray-100"
                 href="https://github.com/npaik"
               >
-                <GithubIcon className="h-6 w-6" />
+                <Image
+                  src="/github.png"
+                  alt="Thread"
+                  width={24}
+                  height={24}
+                  className="h-6 w-6"
+                />
                 <span className="sr-only">GitHub</span>
-              </Link>
-              <Link className="rounded-full p-2 hover:bg-gray-100" href="#">
-                <DribbbleIcon className="h-6 w-6" />
-                <span className="sr-only">Dribbble</span>
               </Link>
               <Link
                 className="rounded-full p-2 hover:bg-gray-100"
                 href="https://www.linkedin.com/in/npaik/"
               >
-                <LinkedinIcon className="h-6 w-6" />
+                <Image
+                  src="/linkedin.png"
+                  alt="LinkedIn"
+                  width={24}
+                  height={24}
+                  className="h-6 w-6"
+                />
                 <span className="sr-only">LinkedIn</span>
+              </Link>
+              <Link
+                href="https://www.threads.net/@nh.paik"
+                className="rounded-full p-2 hover:bg-gray-100"
+              >
+                <Image
+                  src="/thread.png"
+                  alt="Thread"
+                  width={24}
+                  height={24}
+                  className="h-6 w-6"
+                />
+                <span className="sr-only">Thread</span>
               </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-}
-
-function DribbbleIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94" />
-      <path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32" />
-      <path d="M8.56 2.75c4.37 6 6 9.42 8 17.72" />
-    </svg>
-  );
-}
-
-function GithubIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-      <path d="M9 18c-4.51 2-5-2-7-2" />
-    </svg>
-  );
-}
-
-function LinkedinIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-      <rect width="4" height="12" x="2" y="9" />
-      <circle cx="4" cy="4" r="2" />
-    </svg>
   );
 }
