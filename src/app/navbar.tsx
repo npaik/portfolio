4c38c55da7ddfc39830ee82c9873ee04a1ea313b
Tony Paik <nakhyunpaik@gmail.com>
2024-03-25 14:34:10 -0700

    nav bar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -8,12 +8,42 @@ export default function NavBar() {
 
   const closeMenu = () => setIsOpen(false);
 
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const handleMouseEnter = (e: any) => {
+    e.target.style.color = getRandomColor();
+  };
+
+  const handleMouseLeave = (e: any) => {
+    e.target.style.color = "white";
+  };
+
+  const wrapTextForHoverEffect = (text: string) =>
+    text.split("").map((char, index) =>
+      char === " " ? (
+        <span key={index}>&nbsp;</span>
+      ) : (
+        <span
+          key={index}
+          className="letter"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {char}
+        </span>
+      )
+    );
+
   return (
     <div className="fixed top-0 right-0 z-50">
-      <button
-        className="p-4 hover:scale-150"
-        onClick={() => setIsOpen(!isOpen)}
-      >
+      <button className="p-4" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? (
           <Image src="/closebutton.png" alt="Close" width={55} height={55} />
         ) : (
@@ -23,16 +53,16 @@ export default function NavBar() {
       {isOpen && (
         <nav className="fixed top-1/2 left-0 transform -translate-y-1/2 bg-black text-white w-100 h-full flex flex-col items-center justify-center p-8 space-y-6">
           <div className="text-4xl font-bold" onClick={closeMenu}>
-            <Link href="/">Home</Link>
+            <Link href="/">{wrapTextForHoverEffect("Home")}</Link>
           </div>
           <div className="text-7xl font-bold" onClick={closeMenu}>
-            <Link href="/Projects">Projects</Link>
+            <Link href="/Projects">{wrapTextForHoverEffect("Projects")}</Link>
           </div>
           <div className="text-5xl font-bold" onClick={closeMenu}>
-            <Link href="/AboutMe">About Me</Link>
+            <Link href="/AboutMe">{wrapTextForHoverEffect("About Me")}</Link>
           </div>
           <div className="text-6xl font-bold" onClick={closeMenu}>
-            <Link href="/Contact">Contact</Link>
+            <Link href="/Contact">{wrapTextForHoverEffect("Contact")}</Link>
           </div>
         </nav>
       )}
